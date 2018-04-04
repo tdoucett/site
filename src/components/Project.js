9ddbd5b862c56f8d3734ea64318ffa056fd10361
Thomas Doucette <tdoucett@uwaterloo.ca>
2018-04-04 00:30:03 -0400

    Case Study Additions

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class Job extends Component {
+
+    openCaseStudy = () => {
+        window.location = this.props.link;
+    }
+
     render() {
         return (
-            <div className="project">
-                <h3 className="projectName">{this.props.name}</h3>
-                <h4 className="oneLiner">{this.props.oneLiner}</h4>
-                <p className="projectDescription">{this.props.description}</p>
-            </div>
+            <MuiThemeProvider>
+                <div className="project">
+                    <img src={this.props.photo} className={this.props.uuid%2 ? "projectImgLeft" : "projectImgRight" }/>
+                    <h3 className="projectName">{this.props.name}</h3>
+                    <h4 className="oneLiner">{this.props.oneLiner}</h4>
+                    <p className="projectDescription">{this.props.description}</p>
+                    <RaisedButton label="Case Study" labelColor="#f04" buttonStyle={{border: '1px solid #f04'}} onClick={this.openCaseStudy} />
+                </div>
+            </MuiThemeProvider>
         );
     }
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Project from './Project';
+import bump from '../images/bump.png';
 
 class Projects extends Component {
     constructor() {
         super();
-        this.state = { projects: [{uuid: 1, name: 'bump Music', oneLiner: 'Real-time playlist for Bars & Clubs',
-            description: 'Bump Music is an interactive mobile website that lets you vote on upcoming music at your favorite restaurant/bar!',
-            photo: '../images/bump.png'
+        this.state = { projects: [{uuid: 1, name: 'bump Music', oneLiner: 'Real-time Playlist for Bars & Clubs',
+            description: 'Bump Music is an interactive mobile website that lets you vote on upcoming music at your favorite restaurant/bar!' +
+            ' Check it out at bumpmusic.co or read the case study below.',
+            photo: bump,
+            link: 'bump'
         }] }
     }
 
